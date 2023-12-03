ae29381a77ff940c165594a7c876da13a7492ca3
Søren Klintrup <soren@klintrup.dk>
2023-12-03 23:42:43 +0100

    update code

--- .github/scripts/simple-shell-syntax-check.sh
+++ .github/scripts/simple-shell-syntax-check.sh
@@ -1,32 +1,47 @@
-#!/bin/sh
-for file in $(find . -type f -name '*.sh'); do
-  # github actions markdown summary file
-  echo "| file | shell | status | comment |" >> $GITHUB_STEP_SUMMARY
-  echo "| ---- | ----- | :----: | ------- |" >> $GITHUB_STEP_SUMMARY
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+shopt -s globstar
+
+check_shell_syntax() {
+  local file="${1}"
+  local shell="${2}"
+
+  "${shell}" -n "${file}"
+  syntaxexitcode="${?}"
+  if [ "${syntaxexitcode}" -eq 0 ]; then
+    echo "${file} check OK with ${shell}"
+    echo "| \`${file}\` | \`${shell}\` | :white_check_mark: | |" >> "${GITHUB_STEP_SUMMARY}"
+  else
+    echo "${file} check FAIL with ${shell}"
+    echo "| \`${file}\` | \`${shell}\` | :no_entry: | see run log |" >> "${GITHUB_STEP_SUMMARY}"
+    exit ${syntaxexitcode}
+  fi
+}
+
+if [ "${#}" -eq "0" ]; then
+  files=(**/*.sh)
+else
+  files=("$@")
+fi
+
+for file in "${files[@]}"; do
+  echo "| file | shell | status | comment |" >> "${GITHUB_STEP_SUMMARY}"
+  echo "| ---- | ----- | :----: | ------- |" >> "${GITHUB_STEP_SUMMARY}"
   echo "::group::${file}"
   echo "Checking ${file}"
-  
-  # get the shell from the shebang line with support for env
   shell=$(head -n 1 "${file}" | awk 'BEGIN{FS="/"} /^#!\/usr\/bin\/env/{split($NF,a," "); print a[2]; exit} /^#!\//{print $NF; exit}')
-
-  # verify that shell variable is a supported shell
   case ${shell} in
     bash | sh) ;;
     *)
       echo "Unsupported shell: ${shell}"
-      echo "| \`${file}\` | \`${shell}\` | :no_entry: | Unsupported shell |" >> $GITHUB_STEP_SUMMARY
+      echo "| \`${file}\` | \`${shell}\` | :no_entry: | Unsupported shell |" >> "${GITHUB_STEP_SUMMARY}"
       exit 1
       ;;
   esac
-  ${shell} -n "${file}"
-  syntaxexitcode=${?}
-  if [ ${syntaxexitcode} -eq 0 ]; then
-    echo "${file} check OK with ${shell}"
-    echo "| \`${file}\` | \`${shell}\` | :white_check_mark: | |" >> $GITHUB_STEP_SUMMARY
-  else
-    echo "${file} check FAIL with ${shell}"
-    echo "| \`${file}\` | \`${shell}\` | :no_entry: | see run log |" >> $GITHUB_STEP_SUMMARY
-    exit ${syntaxexitcode}
-  fi
+  check_shell_syntax "${file}" "${shell}"
   echo "::endgroup::"
 done
