a27e2142f7e09dc14dbe32a114c2344825d67df8
Søren Klintrup <soren@klintrup.dk>
2023-12-04 00:07:56 +0100

    auto detect shells

--- .github/scripts/simple-shell-syntax-check.sh
+++ .github/scripts/simple-shell-syntax-check.sh
@@ -4,9 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-shopt -s globstar
-
-check_shell_syntax() {
+check_shell_syntax()
+                     {
   local file="${1}"
   local shell="${2}"
 
@@ -22,11 +21,29 @@ check_shell_syntax() {
   fi
 }
 
+check_shells()
+               {
+  local local_shells=("bash" "sh" "ksh" "zsh" "dash" "fish")
+  local local_available_shells=()
+
+  for shell in "${local_shells[@]}"; do
+    if command -v "${shell}" > /dev/null 2>&1; then
+      local_available_shells+=("${shell}")
+    fi
+  done
+
+  IFS="|"
+  echo "${local_available_shells[*]}"
+}
+
 if [ "${#}" -eq "0" ]; then
-  files=(**/*.sh)
+  files=$(find . -type f -name "*.sh" -exec realpath --relative-to=. {} \;)
 else
   files=("$@")
 fi
+
+available_shells=$(check_shells)
+echo "Available shells: ${available_shells}"
 
 for file in "${files[@]}"; do
   echo "| file | shell | status | comment |" >> "${GITHUB_STEP_SUMMARY}"
@@ -35,12 +52,12 @@ for file in "${files[@]}"; do
   echo "Checking ${file}"
   shell=$(head -n 1 "${file}" | awk 'BEGIN{FS="/"} /^#!\/usr\/bin\/env/{split($NF,a," "); print a[2]; exit} /^#!\//{print $NF; exit}')
   case ${shell} in
-    bash | sh) ;;
+    ${available_shells}) ;;
     *)
-      echo "Unsupported shell: ${shell}"
-      echo "| \`${file}\` | \`${shell}\` | :no_entry: | Unsupported shell |" >> "${GITHUB_STEP_SUMMARY}"
-      exit 1
-      ;;
+        echo "Unsupported shell: ${shell}"
+        echo "| \`${file}\` | \`${shell}\` | :no_entry: | Unsupported shell |" >> "${GITHUB_STEP_SUMMARY}"
+        exit 1
+        ;;
   esac
   check_shell_syntax "${file}" "${shell}"
   echo "::endgroup::"
